22c3d12252a4d5841f1aab8d2402fcc7f71393ea
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-26 16:59:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,8 +24,9 @@ def nyc_pigeon_organizer(data)
     attribute_values.each do |attribute, pigeon_names| # Splitting up the attribute and the pigeons in the newly created array
       pigeon_names.each do |names| # Splitting up the names
         if pigeon_list.has_key? names # If the key is in there,
-          binding.pry
+          # binding.pry
           if attribute_values.has_key? attribute
+            binding.pry
             pigeon_list[names][attribute_names] = [attribute] # Add these items to the array
           end
           # binding.pry
